9a402b199e216e067f1f67b51ab012e1e462a43d
michael emmanuel <emmanuelmichaelpk3@gmail.com>
2025-08-14 04:11:34 +0100

    Add search contact and delete contact to the code base

--- main.py
+++ main.py
@@ -17,3 +17,17 @@ def add_contact(name, phone, emai):
 def view_contacts():
     for contact in contacts.find():
         print(f"Name:{contact['name']}, Phone: {contact['phone']}, Email: {contact['email']}")
+
+def search_contact(name):
+    result = contacts.find_one({"name": name})
+    if result:
+        print(f"Name: {result['name']}, Phone:{result['phone']}, Email:{result['email']}")
+    else:
+        print(f"No contact found with name: {name}")
+        
+def delete_contact(name):
+    result = contacts.delete_one({"name": name})
+    if result.deleted_count > 0:
+        print(f"Contact with name: {name} deleted successfully")
+    else:
+        print(f"No contact found with name: {name}")
