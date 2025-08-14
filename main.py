721d5fff942ac0a43cbe92ea2230abf98147ed18
michael emmanuel <emmanuelmichaelpk3@gmail.com>
2025-08-14 04:02:49 +0100

    Add new contact and search for contact to the code base

--- main.py
+++ main.py
@@ -5,3 +5,15 @@ db = client['contact_book']
 contacts = db ['contacts']
 
 
+# FUNCTION FOR CRUD OPERATIONS
+
+# Add a new contact
+def add_contact(name, phone, emai):
+    contact = {'name':name, 'phone':phone,  'email':emai}
+    contacts.insert_one(contact)
+    print(f"Name: {name}  added successfully")
+    
+# view all contacts
+def view_contacts():
+    for contact in contacts.find():
+        print(f"Name:{contact['name']}, Phone: {contact['phone']}, Email: {contact['email']}")
